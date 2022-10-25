correct model loss calculation

common_functions.py
@@ -3,10 +3,16 @@
 
 """imports"""
 
-import os, sys, random
+import os, sys, random, time, glob, re, shutil, io
+from zipfile import ZipFile
+
+import keras.utils
 import numpy as np
 import pandas as pd
 from logger import logger
+
+import asyncio
+import aiofiles
 
 import skimage
 import cv2
@@ -14,8 +20,16 @@ import imageio
 from PIL import Image
 
 from keras import utils
+from keras.preprocessing.image import ImageDataGenerator
 from sklearn import preprocessing as pp
+from sklearn.model_selection import train_test_split
 
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from tensorflow import math
+from keras import layers, models, Model, Input, Sequential, optimizers, losses, callbacks, activations, constraints
 from keras import backend as K
 
 
@@ -51,6 +65,76 @@ def de_standardization(raw_image_array):
     image_array = skimage.util.img_as_ubyte(image_array)
     image_array = np.asarray(image_array).astype("uint8")
     return image_array
+
+
+# weights centralization function
+class WeightCentralization(constraints.Constraint):
+    def __call__(self, w):
+        return w - math.reduce_mean(w)
+
+
+# callback for saving images during model training
+class SaveImageExampleCallback(callbacks.Callback):
+    """
+        callback for saving image while train process
+    """
+    # initialize
+    def __init__(self, test_images_array, path_to_generated_images: str, patience=0):
+        super().__init__()
+
+        # array with images to test model
+        self.test_images_array = test_images_array
+        # path to directory for generated images
+        self.path_to_generated_images = path_to_generated_images
+        # how many epochs been skipped between one image generation test
+        self.patience = patience
+
+        # count of epoch
+        self.count_of_epoch = 0
+        # count of skipped epochs
+        self.count_of_skipped_epoch = 0
+
+        # save original test images for easier comparison
+        self.save_original()
+
+    # save original test images to target directory for easier comparison
+    def save_original(self):
+        for c1, image_array in enumerate(self.test_images_array):
+            p_image_array = de_standardization(image_array)
+            imageio.imwrite(
+                uri=rf"{self.path_to_generated_images}/original_test_image_{c1}.jpeg",
+                im=p_image_array)
+
+    # what's going on in epoch end
+    def on_epoch_end(self, epoch, logs=None):
+        # count epoch
+        self.count_of_epoch += 1
+        self.count_of_skipped_epoch += 1
+
+        # if count of skipped epochs == patience, process images in model and save
+        if self.count_of_skipped_epoch == self.patience:
+            # reset skipped epoch counter
+            self.count_of_skipped_epoch = 0
+
+            # prepare model to image generation
+            generative_model = keras.models.Model(
+                self.model.get_layer(name="encoder_input").input, self.model.get_layer(name="loss_layer").input[1])
+
+            # generate images
+            generated_images = generative_model.predict(self.test_images_array)
+
+            # decode and save images to generated images directory
+            for c1, image_array in enumerate(generated_images):
+                p_image_array = de_standardization(image_array)
+                imageio.imwrite(
+                    uri=rf"{self.path_to_generated_images}/epoch_{self.count_of_epoch}_image_{c1}_.jpeg",
+                                im=p_image_array)
+
+
+
+
+
+
 
 
 

model_test.py
@@ -1,4 +1,4 @@
-# test of VAE model for face generation
+# test of VAE model for face generation with corrected loss calculation
 
 
 """imports"""
@@ -31,38 +31,20 @@ from keras import backend as K
 
 from common_functions import *
 from data_generator import *
-from model import *
+from vae_model import *
 
 
 """params of model"""
 
 path_to_images = r"F:\large_data\Flickr_Faces_HQ_dataset\splitted_images"
-batch_size = 15
+batch_size = 32
 image_array_shape = (128, 128, 3)
 augmentation = False
 
-output_encoder_dim = 20
-reconstruction_loss_factor = 1000
+output_encoder_dim = 10
+reconstruction_loss_factor = 1.0
 
-learning_rate = 0.000001
-steps_per_epoch = 200
-validation_steps = 40
-epochs = 1000
-
-# path to directory for callback outputs
-path_to_callback_generated_images = r"generated_images/callbacks_train_images_3/selu_5_3_5_20_16384_2048_1"
-
-test_images_patience = 1
-model_checkpoint_filepath = "models_checkpoints/VAE_v1.h5"
-model_checkpoint_monitor = "val_loss"
-early_stopping_monitor = "val_loss"
-early_stopping_patience = 100
-RLROP_monitor = "val_loss"
-RLROP_factor = 0.1
-RLROP_patience = 10
-RLROP_min_lr = 0.000000001
-tensorboard_logdir = "tensorboard_logdir/VAE_v1"
-
+steps_per_epoch = 50
 
 """create data generators"""
 
@@ -84,52 +66,37 @@ test_gen = BaseDataGen(path_to_images+r"\test", batch_size, image_array_shape, a
 """create and compile model"""
 
 # create model
-vae_model = VAE(sampling_function=sampling, img_shape=image_array_shape, output_encoder_dim=output_encoder_dim)
+encoder, decoder, vae_model = VAE(sampling_function=sampling, img_shape=image_array_shape,
+                                  output_encoder_dim=output_encoder_dim,
+                                  reconstruction_loss_factor=reconstruction_loss_factor,).model()
 # build model
 vae_model.build(input_shape=[None, image_array_shape[0], image_array_shape[1], image_array_shape[2]])
 # summary of vae
 vae_model.summary()
 # summary of encoder
-vae_model.get_layer("encoder_model").summary()
+encoder.summary()
 # summary of decoder
-vae_model.get_layer("decoder_model").summary()
+decoder.summary()
 
-# initialize loss functions for vae
-vae_loss_func_v1 = loss_func_v1(vae_model.get_layer("encoder_model").get_layer("encoder_mu"),
-                          vae_model.get_layer("encoder_model").get_layer("encoder_log_variance"),
-                          reconstruction_loss_factor=reconstruction_loss_factor)
-vae_loss_func_v2 = loss_func_v2(vae_model.get_layer("encoder_model").get_layer("encoder_mu"),
-                          vae_model.get_layer("encoder_model").get_layer("encoder_log_variance"))
-
-# compile model
-vae_model.compile(optimizer=optimizers.Adam(learning_rate=learning_rate), loss=vae_loss_func_v1,
-            metrics=["accuracy"])
-
-# load weights
-vae_model.load_weights("models_checkpoints/VAE_v1.h5")
+vae_model.load_weights("models_checkpoints/VAE_v1.2.h5")
 
 
 """test, generate and save images"""
 
 # create txt file for values
-f = open(r"generated_images\gen_1\values.txt", mode="w", encoding="utf-8")
+f = open(r"generated_images\gen_3\values.txt", mode="w", encoding="utf-8")
 
 # decode and save generated images
-
-for count in range(0, 1000000):
+for count in range(0, 1000):
     # generate random values for image generation
-    random_values = []
-    for count2 in range(0, output_encoder_dim):
-        random_values.append(float(random.uniform(-10., 10.)))
-    # to numpy array
-    random_values = np.asarray(random_values).astype("float32").reshape(1, output_encoder_dim)
+    random_values = np.random.uniform(low=-3., high=3., size=(1, output_encoder_dim)).astype("float32")
     # generate image
-    generated_image = vae_model.decoder(random_values)
+    generated_image = decoder.predict(random_values, verbose=0)
     # de-standardize
     p_image_array = de_standardization(generated_image[0])
     # write image
     imageio.imwrite(
-        uri=rf"generated_images\gen_1\generated_image_{count}.jpeg",
+        uri=rf"generated_images\gen_3\generated_image_{count}.jpeg",
         im=p_image_array)
     # write value
     f.write(str(random_values.tolist())+"\n")